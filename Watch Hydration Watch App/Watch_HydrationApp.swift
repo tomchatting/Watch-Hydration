73f5114c91b50688f557de650868d76cb93ac3ea
Thomas Chatting <tomchatting@users.noreply.github.com>
2025-05-03 00:05:10 +0100

    Initial Commit

---
+++ Watch Hydration Watch App/Watch_HydrationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Watch_HydrationApp.swift
+//  Watch Hydration Watch App
+//
+//  Created by Thomas Chatting on 03/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct Watch_Hydration_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Watch Hydration Watch AppTests/Watch_Hydration_Watch_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  Watch_Hydration_Watch_AppTests.swift
+//  Watch Hydration Watch AppTests
+//
+//  Created by Thomas Chatting on 03/05/2025.
+//
+
+import Testing
+@testable import Watch_Hydration_Watch_App
+
+struct Watch_Hydration_Watch_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
